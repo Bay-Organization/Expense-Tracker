ee93db9de992b5ae3b6d460cee19a6ab52ec3526
Arslan Rejebov <arslanrejepov174@gmail.com>
2025-11-27 14:42:17 +0800

    schemas/transaction_done

---
+++ expense-tracker/backend/app/schemas/transaction.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import date
+
+class BaseTransaction(BaseModel):
+    amount : float
+    type : str
+    description : str | None = None
+    date  : date
+    category_id : int
+
+class CreateTransaction(BaseTransaction):
+    pass
+
+class ResponseTransaction(BaseTransaction):
+    id : int
+    user_id : int
+
+    class Config:
+        orm_mode=True
+
+
