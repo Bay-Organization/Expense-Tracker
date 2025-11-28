fe19ea992e195881e0b69389a6fb6cca0213540c
Arslan Rejebov <arslanrejepov174@gmail.com>
2025-11-28 18:36:45 +0800

    deps.py_Done_and_Tested_with_main.py

--- expense-tracker/backend/app/main.py
+++ expense-tracker/backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from .database import engine,Base
+from app.database import engine,Base
 from app import models
+from fastapi import Depends
+from app.utils.deps import get_current_user
+from app.models.user import User
 from app.routers import auth
 
 app = FastAPI()
@@ -12,3 +15,11 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     return {"message": "Expense Tracker API is running"}
+
+@app.get("/me")
+def read_me(user: User = Depends(get_current_user)):
+    return {
+        "id": user.id,
+        "username": user.username,
+        "email": user.email
+    }
