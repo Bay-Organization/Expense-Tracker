c743cf6b31b292db0581b5cdcab4630317ebd2ef
Arslan Rejebov <arslanrejepov174@gmail.com>
2025-12-01 18:05:38 +0800

    gitignore_added

--- expense-tracker/backend/app/routers/auth.py
+++ expense-tracker/backend/app/routers/auth.py
@@ -1,28 +1,26 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
 from app.database import get_db
 from app.models.user import User
-from app.schemas.user import CreateUser, ResponseUser
+from app.schemas.user import CreateUser, LoginUser, ResponseUser
 from app.utils.auth import hash_password, verify_password, create_access_token
 
-router = APIRouter(prefix="/auth",tags=["Auth"])
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
 @router.post("/register", response_model=ResponseUser)
-def register(user : CreateUser, db : Session = Depends(get_db)):
+def register(user: CreateUser, db: Session = Depends(get_db)):
 
-    existing=db.query(User).filter(User.email == user.email).first()
+    existing = db.query(User).filter(User.email == user.email).first()
     if existing:
         raise HTTPException(status_code=400, detail="Email already registered")
-    
+
     hashed_pw = hash_password(user.password)
 
-    new_user=User(
+    new_user = User(
         username=user.username,
         email=user.email,
         password_hash=hashed_pw
     )
-
 
     db.add(new_user)
     db.commit()
@@ -30,14 +28,18 @@ def register(user : CreateUser, db : Session = Depends(get_db)):
 
     return new_user
 
-@router.post("/login")
-def login(user:CreateUser , db : Session= Depends(get_db)):
 
-    db_user = db.query(User).filter(User.email == user.email)
+@router.post("/login")
+def login(user: LoginUser, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
 
     if not db_user:
         raise HTTPException(status_code=400, detail="Invalid email or password")
-    
-    token = create_access_token({"sub":str(db_user.id)})
+
+    if not verify_password(user.password, db_user.password_hash):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+
+    token = create_access_token({"sub": str(db_user.id)})
 
     return {"access_token": token, "token_type": "bearer"}
